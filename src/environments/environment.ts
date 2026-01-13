0afb4d4ee6564a49d60f83fbbba185d228dc0186
Wellington Barbosa <wellington_gon@outlook.com>
2026-01-13 08:49:49 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // apiUrl: '/api/',
-  apiUrl: 'ec2-3-137-203-36.us-east-2.compute.amazonaws.com:8055/api/',
+  apiUrl: '/api/',
+  //apiUrl: 'ec2-3-137-203-36.us-east-2.compute.amazonaws.com:8055/api/',
   useMockAuth: false,
 };
